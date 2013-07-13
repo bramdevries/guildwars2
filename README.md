ba3c0fe460ec8babbffdcdb0f33d983cac51b212
Bram Devries <bram_devries@2to1.be>
2013-07-13 16:04:02 +0200

    Added methods for getting world and map names.
    Signed-off-by: Bram Devries <bram_devries@2to1.be>

---
+++ README.md
@@ -0,0 +1,13 @@
+# Guild Wars 2 API
+
+## Roadmap
+
+### V 0.0.1
+* Basic logic for GET requests.
+* Methods for getting the static data (map and world names)
+
+### V 0.0.2
+* Methods for getting events
+
+### V 1.0.0
+* Methods for all endpoints that exist at Saturday 13th July 2013.
--- src/GuildWars2/GuildWars2.php
+++ src/GuildWars2/GuildWars2.php
@@ -3,7 +3,66 @@
 namespace GuildWars2;
 
 class GuildWars2{
-	public function __construct(){
-		echo "[GuildWars2] Construct";
+
+	/**
+	* Base URL for our API requests
+	*/
+	const URL = "https://api.guildwars2.com/";
+
+	const VERSION = "v1";
+
+	/**
+	* What language to return the data in (defaults to english).
+	*/
+	protected $language = "en";
+	protected $supported_languages = array("en", "es", "fr", "de");
+
+	public function __construct($options){
+		$this->setLanguage($options['language']);
+	}
+
+	/**
+	* API Methods
+	*/
+
+	public function maps(){
+		return $this->get('map_names');
+	}
+
+	public function worlds(){
+		return $this->get('world_names');
+	}
+
+	/**
+	* cURL: Get method
+	*/
+
+	private function get($endpoint){
+
+		$url = self::URL . self::VERSION . "/" . $endpoint . ".json?lang=" . $this->getLanguage();
+
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+		curl_setopt($ch, CURLOPT_HEADER, FALSE);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array("Accept: application/json"));
+		$response = curl_exec($ch);
+		curl_close($ch);
+
+		$results = json_decode($response, true);
+		return $results;
+	}
+
+	/**
+	* Language: Getter & Setter
+	*/
+
+	private function setLanguage($lang){
+		if(in_array($lang, $this->supported_languages))
+			$this->language = $lang;
+	}
+
+	public function getLanguage(){
+		return $this->language;
 	}
 }
